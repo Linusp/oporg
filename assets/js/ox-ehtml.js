0c74e9feceb9e17decb0f3b8d59ab427bb521ccd
Joseph Edwards VIII <jedwards8th@gmail.com>
2019-01-03 23:34:56 -0500

    revisiting after an age

--- assets/js/ox-ehtml.js
+++ assets/js/ox-ehtml.js
@@ -6,99 +6,99 @@
 $(document).ready( function(){ set_clickable(); } );
 
 function set_clickable(){
-  oporg_process_outline();      // [JE] calls funcs in oporg.js
+    oporg_process_outline();      // [JE] calls funcs in oporg.js
 
-  // add edit button to every element with class="edit_in_place"
-  $('.edit_button').remove();
-  $('.edit_in_place')
-  .before('<input type="button" value="EDIT" class="edit_button">');
-
-  $('.edit_button').click(function(){
-    var main = $(this).next();
-    var org  = main.next().html();
-    var beg  = main.next().attr("contents-begin");
-    var end  = main.next().attr("contents-end");
-    var html = main.html();
-
-    // TODO: replace with handlebars template
-    var div_str = '<div class="edit_org_widget">'
-      + '<textarea id="editing" class="edit_org">' + org + '</textarea>'
-      + '<div><input type="button" value="SAVE" class="save_button" />'
-      + '<input type="button" value="CANCEL" class="cancel_button" />'
-      + '</div></div>';
-    main.after(div_str);
-    //set_autoresize('textarea#editing');
-
-    // remove all edit buttons & the original html
+    // add edit button to every element with class="edit_in_place"
     $('.edit_button').remove();
-    main.remove();
+    $('.edit_in_place')
+        .before('<input type="button" value="EDIT" class="edit_button">');
 
-    // call these functions when buttons are hit
-    $('.save_button').click(function(){
-      var saveorg = $(this).parent().prev().val();
-      save_changes(this, saveorg, beg, end);
-      //unset_autoresize();
-    });
-    $('.cancel_button').click(function(){
-      abort_changes(this, html);
-      //unset_autoresize();
-    });
-  });
+    $('.edit_button').click(function(){
+        var main = $(this).next();
+        var org  = main.next().html();
+        var beg  = main.next().attr("contents-begin");
+        var end  = main.next().attr("contents-end");
+        var html = main.html();
 
-  // TODO: show edit button on mousover
-  $('.edit_in_place').hover(function(){ $(this).prev().show(); },
-                            function(){ $(this).prev().hide(); } );
-  $('.edit_button').hover(function(){ $(this).show(); },
-                          function(){ $(this).hide(); } );
+        // TODO [JE]: replace with handlebars template
+        var div_str = '<div class="edit_org_widget">'
+            + '<textarea id="editing" class="edit_org">' + org + '</textarea>'
+            + '<div><input type="button" value="SAVE" class="save_button" />'
+            + '<input type="button" value="CANCEL" class="cancel_button" />'
+            + '</div></div>';
+        main.after(div_str);
+        // TODO [JE]: set_autoresize('textarea#editing');
+
+        // remove all edit buttons & the original html
+        $('.edit_button').remove();
+        main.remove();
+
+        // call these functions when buttons are hit
+        $('.save_button').click(function(){
+            var saveorg = $(this).parent().prev().val();
+            save_changes(this, saveorg, beg, end);
+            //unset_autoresize();
+        });
+        $('.cancel_button').click(function(){
+            abort_changes(this, html);
+            //unset_autoresize();
+        });
+    });
+
+    // TODO: show edit button on mousover
+    $('.edit_in_place').hover(function(){ $(this).prev().show(); },
+                              function(){ $(this).prev().hide(); } );
+    $('.edit_button').hover(function(){ $(this).show(); },
+                            function(){ $(this).hide(); } );
 };
 
 function save_changes(obj, org, beg, end){
-  var here = window.location.pathname;
+    var here = window.location.pathname;
 
-  // TODO
-  console.log("begin/end:", beg, end, "org:", org);
-  //return false;
+    // TODO
+    console.log("begin/end:", beg, end, "org:", org);
+    //return false;
 
-  $.ajax({
-    type: 'POST',
-    url: here,
-    data: {org:  org,
-           beg:  beg,
-           end:  end,
-           path: here},
-    statusCode: {
-      200: function(html,status){
-        $(obj).parent().parent().next().after(
-          '<div class="edit_in_place">'+html+'</div>'+
-          '<div class="raw-org" contents-begin="'+beg+
-          '" contents-end="'+end+'">'+org+'</div>');
-        if(typeof(MathJax) !== 'undefined') {
-          MathJax.Hub.Queue(["Typeset", MathJax.Hub,
-                             $(obj).parent().parent().next().next().get()]);
+    $.ajax({
+        type: 'POST',
+        url: here,
+        data: {org:  org,
+               beg:  beg,
+               end:  end,
+               path: here},
+        statusCode: {
+            200: function(html,status){
+                $(obj).parent().parent().next().after(
+                    '<div class="edit_in_place">'+html+'</div>'+
+                        '<div class="raw-org" contents-begin="'+beg+
+                        '" contents-end="'+end+'">'+org+'</div>');
+                if(typeof(MathJax) !== 'undefined') {
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub,
+                                       $(obj).parent().parent().next().next().get()]);
+                }
+                $(obj).parent().parent().next().remove();
+                $(obj).parent().parent().remove();
+                update_offsets(end, (org.length - (Number(end) - Number(beg))));
+                set_clickable();
+            },
+            403: function(){ alert("Unauthorized"); },
+            500: function(error){ alert('error:'+error); }
         }
-        $(obj).parent().parent().next().remove();
-        $(obj).parent().parent().remove();
-        update_offsets(end, (org.length - (Number(end) - Number(beg))));
-        set_clickable();
-      },
-      403: function(){ alert("Unauthorized"); },
-      500: function(error){ alert('error:'+error); }
-    }
-  });
+    });
 };
 
 function abort_changes(obj, old_html){
-  $(obj).parent().parent().after(
-    '<div class="edit_in_place">'+old_html+'</div>');
-  $(obj).parent().parent().remove();
-  set_clickable();
+    $(obj).parent().parent().after(
+        '<div class="edit_in_place">'+old_html+'</div>');
+    $(obj).parent().parent().remove();
+    set_clickable();
 };
 
 function update_offsets(end, delta){
-  $('.raw-org').map(function(){
-    var old_beg = Number($(this).attr('contents-begin'));
-    if(old_beg >= end) { $(this).attr('contents-begin', (old_beg + delta)); }
-    var old_end = Number($(this).attr('contents-end'));
-    if(old_end >= end) { $(this).attr('contents-end', (old_end + delta)); }
-  });
+    $('.raw-org').map(function(){
+        var old_beg = Number($(this).attr('contents-begin'));
+        if(old_beg >= end) { $(this).attr('contents-begin', (old_beg + delta)); }
+        var old_end = Number($(this).attr('contents-end'));
+        if(old_end >= end) { $(this).attr('contents-end', (old_end + delta)); }
+    });
 };
--- assets/oporg/js/oporg.js
+++ assets/oporg/js/oporg.js
@@ -3,97 +3,100 @@
 // Adds OpOrg functionality to org-ehtml and theme assets
 
 var oporg_outline = {
-  'outline-2': []
+    'outline-2': []
 };
 
 $(function() {
-  $('.title').prepend('<i class="fa fa-connectdevelop logo"></i>');
+    $('.title').prepend('<i class="fa fa-connectdevelop logo"></i>');
 });
 
 function concat_outline_text(obj) {
-  var eip_l = $(obj).find('.edit_in_place');
-  var raw_l = $(obj).find('.raw-org');
+    var eip_l = $(obj).find('.edit_in_place');
+    var raw_l = $(obj).find('.raw-org');
 
-  if (eip_l.length > 1 && raw_l.length > 1) {
-    var first_eip = eip_l.eq(0);
-    var first_raw = raw_l.eq(0);
+    if (eip_l.length > 1 && raw_l.length > 1) {
+        var first_eip = eip_l.eq(0);
+        var first_raw = raw_l.eq(0);
 
-    for (i=1; i<eip_l.length; i++) {
-      first_eip.append(eip_l.eq(i).html());
-      first_raw.append(raw_l.eq(i).html());
-      first_raw.attr('contents-end', raw_l.eq(i).attr('contents-end'));
-      eip_l.eq(i).remove();
-      raw_l.eq(i).remove();
+        for (i=1; i<eip_l.length; i++) {
+            first_eip.append(eip_l.eq(i).html());
+            first_raw.append(raw_l.eq(i).html());
+            first_raw.attr('contents-end', raw_l.eq(i).attr('contents-end'));
+            eip_l.eq(i).remove();
+            raw_l.eq(i).remove();
+        }
     }
-  }
 }
 
 // From http://www.impressivewebs.com/textarea-auto-resize/
 function set_autoresize(elt) {
-  var txt = $(elt),
-      hiddenDiv = $(document.createElement('div')),
-      content = null;
+    var txt = $(elt),
+        hiddenDiv = $(document.createElement('div')),
+        content = null;
 
-  // txt.addClass('txtstuff');  // <-- WHY?!?
-  hiddenDiv.addClass('hiddendiv common');
-  $('body').append(hiddenDiv);
+    // txt.addClass('txtstuff');  // <-- WHY?!?
+    hiddenDiv.addClass('hiddendiv common');
+    $('body').append(hiddenDiv);
 
-  // match widths
-  txt.outerWidth(txt.parent().width());
-  hiddenDiv.width(txt.width());
+    // match widths
+    txt.outerWidth(txt.parent().width());
+    hiddenDiv.width(txt.width());
 
-  txt.on('keyup', function () {
-    content = $(this).val();
-    content = content.replace(/\n/g, '<br>');
-    hiddenDiv.html(content + '<br class="lbr">');
-    $(this).css('height', hiddenDiv.height());
-  });
+    txt.on('keyup', function () {
+        content = $(this).val();
+        content = content.replace(/\n/g, '<br>');
+        hiddenDiv.html(content + '<br class="lbr">');
+        $(this).css('height', hiddenDiv.height());
+    });
 }
 
 function unset_autoresize() {
-  $('.hiddendiv.common').remove();
+    $('.hiddendiv.common').remove();
 }
 
 function oporg_process_outline() {
-  //handle special outline-2 (tabs)
-  $('.outline-2').each(function(){
-    oporg_outline['outline-2'].push({
-      'level': 2,
-      'id': $(this).attr('id'),
-      'elt': this,
-      'maxdepth': 12
+    //handle special outline-2 (tabs)
+    $('.outline-2').each(function(){
+        oporg_outline['outline-2'].push({
+            'level': 2,
+            'id': $(this).attr('id'),
+            'elt': this,
+            'maxdepth': 12
+        });
     });
-  });
 
-  for (var i=0; i < oporg_outline['outline-2'].length; i++) {
-    var ol2_i = oporg_outline['outline-2'][i];
+    for (var i=0; i < oporg_outline['outline-2'].length; i++) {
+        var ol2_i = oporg_outline['outline-2'][i];
 
-    for (var olix=3; olix <= ol2_i['maxdepth']; olix++) {
-      var subtree = $(ol2_i['elt']).find('.outline-'+olix);
-      if (subtree.length === 0) continue;
+        for (var olix=3; olix <= ol2_i['maxdepth']; olix++) {
+            var subtree = $(ol2_i['elt']).find('.outline-'+olix);
+            if (subtree.length === 0) continue;
 
-      if (!oporg_outline['outline'+olix])
-        oporg_outline['outline-'+olix] = [];
+            if (!oporg_outline['outline'+olix])
+                oporg_outline['outline-'+olix] = [];
 
-      subtree.each(function(){
-        // concat consecutive raw-org divs in the outline-text-N
-        subtree.find('div[class^="outline-text-"]').each(function(){
-          concat_outline_text(this);
-        });
+            subtree.each(function(){
+                // concat consecutive raw-org divs in the outline-text-N
+                subtree.find('div[class^="outline-text-"]').each(function(){
+                    concat_outline_text(this);
+                });
 
-        // make_sortable outline-container-orgheadline[N]
-        $(this).addClass('sortable');
+                // make_sortable outline-container-orgheadline[N]
+                $(this).addClass('sortable');
 
-        // adding to the outline object
-        oporg_outline['outline-'+olix].push({
-          'level': olix,
-          'id': $(this).attr('id'),
-          'elt': this
-        });
-      });
+                // adding to the outline object
+                oporg_outline['outline-'+olix].push({
+                    'level': olix,
+                    'id': $(this).attr('id'),
+                    'elt': this
+                });
+            });
+        }
     }
-  }
 
-  // activate sortable
-  $('.sortable').sortable();
+    // TODO
+    console.log(oporg_outline);
+
+    // activate sortable
+    $('.sortable').sortable();
 }
